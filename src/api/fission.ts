d26580944864eb06b121d6b7df2c2120d1511756
chenh.0307 <chenh.0307@bytedance.com>
2025-09-20 17:29:10 +0800

    update getNamespace

--- src/api/fission.ts
+++ src/api/fission.ts
@@ -3,7 +3,7 @@ import request from '../utils/request';
 // 获取指定命名空间下的所有函数
 export const getFunctionsByNamespace = (namespace: string) => {
   return request({
-    url: `/api/fission/v1/namespaces/${namespace}/functions`,
+    url: `http://127.0.0.1:30081/api/fission/v1/namespaces/${namespace}/functions`,
     method: 'get',
   });
 };
